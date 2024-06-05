f01f5866dad986c464640a1f70c940684c74a21d
Rajiv Kushwaha <raj25by10@gmail.com>
2024-06-05 16:33:51 +0530

    Refactor nvim-treesitter config to its own file

--- nvim/lua/rajiv/plugins.lua
+++ nvim/lua/rajiv/plugins.lua
@@ -59,24 +59,7 @@ local lazy_plugins = {
     {
         "nvim-treesitter/nvim-treesitter",
         lazy = false,
-        config = function()
-            require("nvim-treesitter.configs").setup({
-              ensure_installed = { "go", "lua", "vim", "c", "vimdoc"},
-              sync_install = false,
-              highlight = {
-                enable = true,
-
-                disable = function(lang, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-                end,
-                additional_vim_regex_highlighting = false,
-              }
-          })
-      end
+        config = get_setup("nvim-treesitter")
     }
 }
 
