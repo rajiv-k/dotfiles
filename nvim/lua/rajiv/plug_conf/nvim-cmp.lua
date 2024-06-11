64f2f88a7b1f62069c958c9c2af7fea0414e2e93
Rajiv Kushwaha <raj25by10@gmail.com>
2024-06-11 18:05:27 +0530

    neovim: Enable function signature help

--- nvim/lua/rajiv/plug_conf/nvim-cmp.lua
+++ nvim/lua/rajiv/plug_conf/nvim-cmp.lua
@@ -11,6 +11,7 @@ cmp.setup({
       { name = "nvim_lsp" },
       { name = "buffer" },
       { name = "path" },
+      { name = "nvim_lsp_signature_help" },
       -- { name = "cmdline" },
     })
 })
